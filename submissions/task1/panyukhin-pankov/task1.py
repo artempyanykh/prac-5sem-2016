86dafed67509d78dffecd8117336491bff71f1e2
Dariy Pankov <pnkdariy@gmail.com>
2016-11-20 18:14:23 +0300

    new file:   submissions/task1/panyukhin-pankov/README.md 	new file:   submissions/task1/panyukhin-pankov/task1.ipynb 	new file:   submissions/task1/panyukhin-pankov/task1.py

---
+++ submissions/task1/panyukhin-pankov/task1.py
@@ -0,0 +1,15 @@
+from scipy.optimize import linprog;
+import numpy as np;
+ 
+def nash_equilibrium(inMatrix):
+    inMatrix = np.array(inMatrix);
+    mmin = inMatrix[inMatrix != 0].min();
+    delta = abs(mmin) + 1;
+    inMatrix = inMatrix + delta;
+    transp = inMatrix.T*-1;
+    c = np.ones((len(inMatrix)));
+    bub = np.ones((len(inMatrix[0])))*-1;
+    resz = linprog(c, transp, bub);
+    resw = linprog(bub, inMatrix, c);
+    v = 1 / sum(resz.x);
+    return v * resz.x, v * resw.x, v - delta;
