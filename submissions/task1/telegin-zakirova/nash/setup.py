237d8ba81bf0478e60ec72f1e40423c1143a95ca
konstantin013 <konstantin013@users.noreply.github.com>
2016-11-24 20:21:05 +0300

    task1-telegin-zakirova (#56)

---
+++ submissions/task1/telegin-zakirova/nash/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'nash',
+    version = '0.1dev',
+    packages = ['nash_task',],
+    license = 'MIT',
+    long_descriptions = open('README.txt').read(),
+)
